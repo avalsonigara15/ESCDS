bf76aff3f06a05d79b30b7c274a6e5198a494857
avalsonigara15 <avals1504@gmail.com>
2023-07-23 16:25:11 +0530

    ECDSA

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,14 +1,26 @@
 import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import { utf8ToBytes } from "ethereum-cryptography/utils";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({
+  address,
+  setAddress,
+  balance,
+  setBalance,
+  privateKey,
+  setPrivateKey,
+}) {
   async function onChange(evt) {
     const address = evt.target.value;
     setAddress(address);
     if (address) {
       const {
-        data: { balance },
+        data: { balance, privateKey },
       } = await server.get(`balance/${address}`);
       setBalance(balance);
+      setPrivateKey(privateKey);
     } else {
       setBalance(0);
     }
@@ -19,8 +31,16 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Wallet address
+        <input
+          placeholder="Type your wallet address"
+          value={privateKey}
+          onChange={onChange}
+        ></input>
+      </label>
+
+      <label>
+        Address <input type="text" value={address.slice(-20)}></input>
       </label>
 
       <div className="balance">Balance: {balance}</div>
